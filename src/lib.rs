f85be121dddc9e4399768a829d29ec732204ed70
andrew-cheng <andrew.cheng@gmail.com>
2023-10-05 11:02:13 -0500

    make it concurrent

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use futures::{stream, StreamExt};
 use reqwest::{self, header};
 
 mod html_fmter;
 use html_fmter::fmt_html;
+
+const CONCURRENT_REQUESTS: usize = 15;
 
 pub async fn request_data(
     start_ch: i32,
@@ -18,6 +21,7 @@ pub async fn request_data(
         .default_headers(headers)
         .build()?;
 
+    let mut urls = Vec::with_capacity((end_ch - start_ch + 1) as usize);
     let mut combined_txt = String::new();
     for chapter in start_ch..=end_ch {
         let request_url = format!(
@@ -25,10 +29,40 @@ pub async fn request_data(
             ncode = ncode.trim()
         );
 
-        let res = client.get(&request_url).send().await?.text().await?;
+        urls.push(request_url);
 
-        combined_txt.push_str(&fmt_html(&res));
+        combined_txt.push_str(&format!("%%%{}&&&", chapter - 1));
+
+        // let res = client.get(&request_url).send().await?.text().await?;
+
+        // combined_txt.push_str(&fmt_html(&res));
     }
+
+    let fetches = stream::iter(urls)
+        .enumerate()
+        .map(|(idx, url)| {
+            let client = &client;
+            async move {
+                match client.get(&url).send().await {
+                    Ok(resp) => match resp.text().await {
+                        Ok(text) => {
+                            return (idx, fmt_html(&text));
+                        }
+                        Err(_) => println!("ERROR reading {}", url),
+                    },
+                    Err(_) => println!("ERROR downloading {}", url),
+                }
+                (idx, String::from("failed"))
+            }
+        })
+        .buffer_unordered(CONCURRENT_REQUESTS)
+        .collect::<Vec<(usize, String)>>();
+
+    let results = fetches.await;
+
+    results.iter().for_each(|(idx, content)| {
+        combined_txt = combined_txt.replace(&format!("%%%{}&&&", idx), content);
+    });
 
     Ok(combined_txt)
 }
